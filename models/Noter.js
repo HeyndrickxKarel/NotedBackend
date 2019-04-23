cbd3853c62540be423d1e45b7b625b463308ca95
HeyndrickxKarel <karelhey@skynet.be>
2019-04-23 14:24:11 +0200

    connection fix

--- models/Noter.js
+++ models/Noter.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 var NoterShema = new mongoose.Schema({
     firebaseUserId : String,
     notes: [Object]
-})
+},{collection:'noters'})
 mongoose.model('Noter',NoterShema);
